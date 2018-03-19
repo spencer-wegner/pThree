8ac8fd4b690a9f1dc15ade6f56444928e9df2240
sabrinatouchCU <sabrina.touch@colorado.edu>
2018-03-19 14:13:47 -0600

    Changed sections in tables, changed necessary primary and foregin key

--- project_files/Back-End/p3_database.sql
+++ project_files/Back-End/p3_database.sql
@@ -1,39 +1,34 @@
 /*--- Sabrina Touch ---*/
-/* Last Update: 3/12/2018 */
-
-/***
-	SIDE NOTES
- 	- Will probably need to use FOREIGN KEY instead of PRIMARY KEY.
-  ***/
+/* Last Update: 3/19/2018 */
 
 /* TO CREATE THE DATABASE CALLED 'p3_database' */
-CREATE database p3_database;
+CREATE database p3_database CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 use p3_database; 
 
 
 /* TABLE #1 */
-CREATE TABLE if not exists user_login (
-	userID 		int(1) 		NOT NULL	auto_increment, 
+CREATE TABLE if not exists user_login ( 
 	username 	varchar(255)	NOT NULL,
 	password 	varchar(255)	NOT NULL,
 
-	PRIMARY KEY(userID) 	
-) CHARACTER SET utf8 COLLATE utf8_general_ci;
+	PRIMARY KEY(username) 	
+);
 
 /* user_login data will need to be received and stored */
-INSERT INTO user_login (userID, username, password) VALUES
-('1', 'TestUsername1', 'TestPassword1'),
-('2', 'TestUsername2', 'TestPassword2');
+INSERT INTO user_login (username, password) VALUES
+('TestUsername1', 'TestPassword1'),
+('TestUsername2', 'TestPassword2');
 
 /* TABLE #2 */
 CREATE TABLE if not exists playlist (
-	userID 		int(1)		NOT NULL	auto_increment,
+	username	varchar(255)	NOT NULL,
 	url 		varchar(255)	NOT NULL,
 
-	PRIMARY KEY(userID)
-) CHARACTER SET utf8 COLLATE utf8_general_ci;
+	FOREIGN KEY(username) REFERENCES user_login(username)
+);
 
 /* playlist data will need to be generated, received, and stored */
-INSERT INTO playlist (userID, url) VALUES
-('1', 'www.testplaylistlink1.com'),
-('2', 'www.testplaylistlink2.com');
+INSERT INTO playlist (username, url) VALUES
+('TestUsername1', 'www.testplaylistlink1-1.com'),
+('TestUsername1', 'www.testplaylistlink1-2.com'),
+('TestUsername2', 'www.testplaylistlink2-1.com');
