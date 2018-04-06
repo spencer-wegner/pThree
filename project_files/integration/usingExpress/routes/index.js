99735cea8d00abe4a0b542165d9fff14d31b0b7c
lowlyOz <garrison.lee@colorado.edu>
2018-04-06 12:14:12 -0600

    create account is working, minus the check existing account portion, I'll add it soon.

--- project_files/integration/usingExpress/routes/index.js
+++ project_files/integration/usingExpress/routes/index.js
@@ -12,8 +12,22 @@ router.get('/p3Main.html', function(req, res, next) {
 	res.sendFile('p3Main.html', { root: __dirname});
 });
 
+router.get('/p3Submit.html', function(req, res, next) {
+	res.sendFile('p3Submit.html', {root: __dirname});
+});
+
 router.get('/p3Create.html', function(req, res, next) {
 	res.sendFile('p3Create.html', { root: __dirname});
+});
+
+router.post('/p3Create.html', function(req, res, next) {
+	var Nombre = req.body.username;
+	var ShallPass = req.body.password;
+	var sql = mysql.format("INSERT INTO user_login (username, password) VALUES (?,?)",[Nombre,ShallPass]);
+	connection.query(sql, function(err,rows) {
+		if ( err ) throw err;
+		res.redirect('p3Login.html')
+	});
 });
 
 router.get('/p3About.html', function(req, res, next) {
@@ -43,7 +57,7 @@ router.post('/p3Login.html', function(req,res, next) {
 			res.sendFile('p3Login.html', {root: __dirname});
 		}
 	});
-})
+});
 
 router.get('/p3LogoColored.png', function(req, res, next) {
 	res.sendFile('p3LogoColored.png', { root: __dirname});
