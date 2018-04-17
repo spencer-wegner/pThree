51f21ae4548ed1b8f8c107b72c2308c9c97ffaa6
Christoph98 <chuh0424@colorado.edu>
2018-04-16 18:58:23 -0600

    Added the basic authorization code

---
+++ project_files/frontEnd/server.js
@@ -0,0 +1,11 @@
+/* Load the HTTP library */
+
+var http = require("http");
+
+/* Create an http server to handle responses */
+
+http.createServer(function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
+}).listen(8888);
